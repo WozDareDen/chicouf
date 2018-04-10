0b9c7dfaf60821672e18d0eefae458c26e8395bb
Florent SADOT <fsadot@561557n.ac-rennes.fr>
2018-04-10 14:32:56 +0200

    mergError

--- app/Public/js/errorChecking.js
+++ app/Public/js/errorChecking.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
         }
     });
 });
+
 // EMAIL VALIDATION
 var $mdp = $('#regFormPass');
 var regPassTest = $mdp.value;
--- app/Views/frontend/registrationView.php
+++ app/Views/frontend/registrationView.php
@@ -137,12 +137,6 @@
 
 
 
-
-
-
-
-
-
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="app/Public/js/errorChecking.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
